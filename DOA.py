db11fee4e260ffbfec5d8c745d334abc5d889555
jimwang418 <wangwang.0418@hotmail.com>
2022-11-27 17:40:09 -0500

    Change to baud rate and delay

--- DOA.py
+++ DOA.py
@@ -5,7 +5,7 @@ import time
 import serial
 
 dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
-arduino = serial.Serial(port='COM5', baudrate=9600, timeout=10)
+arduino = serial.Serial(port='COM5', baudrate=115200, timeout=10)
 
 if dev:
     Mic_tuning = Tuning(dev)
@@ -13,6 +13,6 @@ if dev:
     while True:
         try:
             arduino.write((str(Mic_tuning.direction) + ' ' + str(int(Mic_tuning.loudness()))).encode('utf-8'))
-            time.sleep(1)
+            time.sleep(0.05)
         except KeyboardInterrupt:
             break
