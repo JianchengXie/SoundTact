3817ddea3a25b6d2e2a92d2bb9da4e479177fb92
Joe X <jxie46@jhu.edu>
2022-11-27 17:31:07 -0500

    DOA.py: serial.write(DOA loudness)

--- DOA.py
+++ DOA.py
@@ -2,15 +2,17 @@ from tuning_soundTact import Tuning
 import usb.core
 import usb.util
 import time
+import serial
 
 dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
+arduino = serial.Serial(port='COM5', baudrate=9600, timeout=10)
 
 if dev:
     Mic_tuning = Tuning(dev)
-    print(str(Mic_tuning.direction) + ' ' + str(int(Mic_tuning.loudness())))
+    arduino.write((str(Mic_tuning.direction) + ' ' + str(int(Mic_tuning.loudness()))).encode('utf-8'))
     while True:
         try:
-            print(str(Mic_tuning.direction) + ' ' + str(int(Mic_tuning.loudness())))
+            arduino.write((str(Mic_tuning.direction) + ' ' + str(int(Mic_tuning.loudness()))).encode('utf-8'))
             time.sleep(1)
         except KeyboardInterrupt:
             break
